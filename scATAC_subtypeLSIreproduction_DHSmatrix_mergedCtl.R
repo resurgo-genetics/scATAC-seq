e69ad170ad74447f862426dca4ebba0bc7eeeecc
cass <cassandraburd@yahoo.com>
2017-01-27 15:52:56 -0500

    preliminary code using lsa package and some of my own math

--- scATAC_subtypeLSIreproduction_DHSmatrix_mergedCtl.R
+++ scATAC_subtypeLSIreproduction_DHSmatrix_mergedCtl.R
@@ -1,12 +1,10 @@
 library(data.table)
 library(proxy)
+library(lsa)
 setwd("~/Documents/LeslieLab/CusanovichData_Processed")
 
 #original code
-#DHSmatrix = fread('CtlSetMerged.dhsmatrix.txt',header=F,stringsAsFactors = F,sep =",")
-#saveRDS(DHSmatrix, 'CtlSetMerged.dhsmatrix.RDS')
-#DHSmatrix = readRDS('CtlSetMerged.dhsmatrix.RDS')
-DHSmatrix = readRDS('CtlSetMerged.dhsmatrix.GM12878only.RDS')
+DHSmatrix = fread('CtlSetMerged.dhsmatrix.GMfullyreduced.csv',header=F,stringsAsFactors = F,sep ="\t")
 barcodes = cbind(as.character(t(DHSmatrix[1,])),c(-4:(ncol(DHSmatrix)-5)))[6:ncol(DHSmatrix),]
 barcodes = as.data.frame(barcodes,stringsAsFactors = F)
 setnames(barcodes,c("Barcode","Order"))
@@ -16,19 +14,21 @@ Assignments = rbind(Assignments,read.delim('GSM1647125_CtlSet2.readcounts.txt',h
 Assignments = rbind(Assignments,read.delim('GSM1647126_CtlSet3.readcounts.txt',header=T))
 Assignments = merge(barcodes,Assignments,by='Barcode',all.x=T)
 
+#using LSA package; i don't think this mimics what is done in the paper exactly
+tfmatrix = as.textmatrix(DHSmatrix)
+weightedmatrix = lw_tf(tfmatrix) * gw_idf(tfmatrix)
 
-#remove cells with < 400 and sites with < 150
-#if you want to subset just cell types
-#DHSmatrix = subset(DHSmatrix,select=(Assignments$CellTypeAssignment=="GM12878" & !is.na(Assignments$CellTypeAssignment)))
-#rm(DHSmatrix)
-#saveRDS(DHSmatrix1, 'CtlSetMerged.dhsmatrix.GM12878only.RDS')
-DHSmatrix1 = sapply(DHSmatrix, as.numeric)
-DHSmatrix1 = DHSmatrix[rowSums(DHSmatrix)>=150,]
-DHSmatrix2 = DHSmatrix1[,colSums(DHSmatrix1)>=400]
-saveRDS(DHSmatrix2, 'CtlSetMerged,dhsmatrix.GM12878only.reduced.RDS')
-distances = dist(DHSmatrix2,method="jaccard",by_rows=F)
-fit <- cmdscale(distances,eig=TRUE, k=2)
-x <- fit$points[,1]
-y <- fit$points[,2]
-plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",main="GM12878 Subtypes, Jaccard Distance, DHS Peaks")
-legend('bottomleft', pch=c(1), col=1, 'GM12878', bty='n', cex=.9, box.col='darkgreen')
+#something to transform scores to -1.5 +1.5 scale?
+weightedsvd = svd(weightedmatrix, nu = 6, nv = 6)
+
+
+#my own code to perform LSA as closely as possible to what they do in the paper
+  #create vector of total site usage per cell
+  numsites = colSums(DHSmatrix)
+  #divide each column by this value (or the log of it?)
+  weightedDHSmatrix = DHSmatrix/colSums
+  #weightedDHSmatrix = DHSmatrix/log10(colSums)
+  
+  
+
+
