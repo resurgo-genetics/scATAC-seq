08d114ae52b0f20c3de401e6da06dd7fee965937
cass <cassandraburd@yahoo.com>
2017-01-27 15:51:20 -0500

    fixed bugs due to misplaced columns from initial merge

--- scATACseq_subsetmergedCtl.py
+++ scATACseq_subsetmergedCtl.py
@@ -1,36 +1,37 @@
 import numpy as np
 
-inputfile = open("CtlSetMerged.dhsmatrix.txt",'r')
-outputfile = open("CtlSetMerged.dhsmatrix.GM12878only_rowreduced.csv",'w')
+inputfile = open("CtlSetMerged.dhsmatrix_dataonly.txt",'r')
+outputfile = open("CtlSetMerged.dhsmatrix.rowreduced_fixed.csv",'w')
+
+def subsetcells(line):
+    out = []
+    for i in line[5:]:
+        if len(i) >= 1:
+            out.append(int(i))
+        elif len(i) == 0:
+            out.append(0)
+    return(out)
+
+#header
+line = inputfile.readline().split(',')
+columns = [0] * (len(line)-5); line1 = line[1:4]; line[-1] = line[-1][0:-1]
+for i in line:
+    if len(i) == 36:
+        line1.append(i)
+outputfile.write("\t".join(line1))
 
 
-columns = [0] * (len(inputfile.readline().split(','))-10)
 inputfile.seek(0)
 for line in inputfile.readlines()[1:]:
-    line = np.array(line.split(','))
-    try:
-        line[np.where(line=="")] = '0'
-        line = list(line[0:len(line)-1])
-    except:
-        line = list(line[0:len(line)-1])
-    line1 = line[5:4542] + line[4544:9124] + line[9126:13841]
-    line1 = map(int,line1)
-    columns = np.add(columns,line1)
+    line = line.split(',')
+    line[-1] = line[-1][0:-1]
+    line1 = subsetcells(line)
+    columns = np.add(columns,np.array(line1))
     if np.sum(line1) >= 150:
         outputfile.write("\t".join(line[1:4] + map(str,line1)) +'\n')
-  
 inputfile.close()
 outputfile.close()
 
-columns = np.array(columns)
-columns = (columns >= 400)
-
-inputfile = open("CtlSetMerged.dhsmatrix.GM12878only_rowreduced.csv",'r')
-outputfile = open("CtlSetMerged.dhsmatrix.GM12878only_reduced.csv",'w')
-
-for line in inputfile.readlines():
-    line = map(int,line.split(',')))
-    outputfile.write('\t'.join(line[columns]))
     
     
     
