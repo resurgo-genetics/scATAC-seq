4a9528e5ebb11cfc0262d0857993da460d628d7e
cass <cassandraburd@yahoo.com>
2017-01-30 18:49:17 -0500

    editted to include MDS code at the end

--- scATAC_jaccardtsne_DHSmatrix_mergedCtl.R
+++ scATAC_jaccardtsne_DHSmatrix_mergedCtl.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(proxy)
 library(Rtsne)
+library(amap)
 setwd("~/Documents/LeslieLab/CusanovichData_Processed")
 
 DHSmatrix = fread('CtlSetMerged.dhsmatrix.GMfullyreduced.csv',header=F,stringsAsFactors = F,sep= " ")
@@ -14,13 +15,27 @@ annots = unique(annots,by=c('V1','V2','V3'))
 annots = merge(annots,sites,by=c("V1",'V2','V3'))
 annots = annots[order(as.numeric(annots$order)),]
 
-#on cells
+#t-sne on cells
 distances = dist(DHSmatrix,method="jaccard",by_rows=F)
+#distances = Dist(t(DHSmatrix),method='binary')
 tsne1 = Rtsne(distances,is_distance=T,verbose=T)
 plot(tsne1$Y[,1],tsne1$Y[,2], xlab = "",ylab='',main="Clustering of GM12878 Subtypes by Site Usage")
 
-#on sites
-distances = dist(DHSmatrix,method="jaccard",by_rows=T)
-tsne2 = Rtsne(distances,is_distance=T,verbose=T)
+#t-sne on sites
+distances2 = dist(DHSmatrix,method="jaccard",by_rows=T)
+tsne2 = Rtsne(distances2,is_distance=T,verbose=T)
 plot(tsne2$Y[,1],tsne2$Y[,2], xlab = "",ylab='',col=annots$V8,main="Clustering of Accessible Sites by GM12878 Subtype Coordination")
+
+#MDS on cells
+fit <- cmdscale(distances,eig=TRUE, k=2)
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",main="Clustering of GM12878 Subtypes by Site Usage")
+
+#MDS on sites
+fit <- cmdscale(distances2,eig=TRUE, k=2)
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",main="Clustering of Accessible Sites by GM12878 Subtype Coordination")
+
 
